3ea74f177feaf13a0da527f53795a498d1daba96
sakshisahu07 <sakshisahu072@gmail.com>
2024-07-07 22:18:23 -0700

    UseEffect

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,22 @@
 // this is the exapmple of useeffect//
 
 
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+// const App=()=>{
+//   const[cnt,setCnt]=useState(0);
+// useEffect(()=>{
+//   setTimeout(()=>{
+//     setCnt(cnt+1);
+//   },5000)
+// },[])
+// return(
+//   <>
+//   <h1> this is there method of use effect {cnt}</h1>
+//   </>
+// )
+// }
+// export default App;
 
-
-const App=()=>{
-
-
-  const[cnt,setCnt]=useState(0);
-
-
-useEffect(()=>{
-  setTimeout(()=>{
-    setCnt(cnt+1);
-  },5000)
-},[])
-
-
-
-return(
-  <>
-  <h1> this is there method of use effect {cnt}</h1>
-  </>
-)
-
-
-}
-
-export default App;
 
 
 
@@ -43,12 +32,9 @@ const App=()=>{
 useEffect(()=>{
   setMulti(cnt*2)
 },[cnt])
-
-
-
  return(
   <>
- <h1> this is the effentn method {cnt}</h1>
+ <h1> this is the useeffect method {cnt}</h1>
  <h1> this is the multiplication {multi}</h1>
  <button onClick={()=>{setCnt(cnt+1)}}>CLICK HERE</button>
   </>
@@ -57,55 +43,6 @@ useEffect(()=>{
 
 export default App;
 
-// ====================================================================================================
 
-import { useState, useEffect } from "react"
-import axios from "axios"
-const App=()=>{
-  const [mydata, setMydata]=useState ([]);
-  const loadData=()=>{
-    let url="https://jsonplaceholder.typicode.com/todos/";
-    axios.get(url).then((res)=>{
-      setMydata(res.data);
-    });
-  }
-  useEffect(()=>{
-    loadData();
-  },[])
 
-  let status="";
-  const ans=mydata.map((key)=>{
-    if(key.completed==true){
-      status="TRUE"
-    }
-    else{
-      status="false"
-    }
-  return(
-    <>
-    <tr>
-      <td>{key.userId}</td>
-      <td>{key.id}</td>
-      <td>{key.id}</td>
-      <td>{status}</td>
-    </tr>
-    </>
-  )
-})
-return(
-  <>
-  <h1>Welcome to cybrom</h1>
-  <table>
-    <tr>
-      <th>User ID</th>
-      <th>ID</th>
-      <th>Title</th>
-      <th>Completed</th>
-    </tr>
-    {ans}
-  </table>
-  </>
-)
-}
-export default App;
 
