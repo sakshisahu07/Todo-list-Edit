12219b39a67bde94f3176ee8667f5ad85b416a55
sakshisahu07 <sakshisahu072@gmail.com>
2024-07-21 16:58:35 -0700

    Context API

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
-import Comp1 from "./Comp1";
-
+import Cybrom from "./Cybrom";
+import { createContext } from "react";
+const myContext=createContext();
 const App=()=>{
-    const[user, setUser]=useState("Sakshi!!")
-    return(
-      <>
-      <h1>Welcome {user}</h1>
-      <Comp1 user={user}/>
-      </>
-    )
-  }
-  export default App;
+  const[user,setUser]=useState("Sakshi")
+  return(
+    <>
+    <h1>Welcome to bhopal:{user}</h1>
+    <myContext.Provider value={{user}}>
+      <Cybrom/>
+    </myContext.Provider>
+    </>
+  )
+}
+export default App;
+export {myContext}
---
+++ src/Cybrom.jsx
@@ -0,0 +1,17 @@
+
+
+
+
+import College from "./College";
+
+
+const Cybrom=()=>{
+    return(
+      <>
+      <h1>Welcome to Cybrom</h1>
+      <College/>
+      
+      </>
+    )
+  }
+  export default Cybrom;
