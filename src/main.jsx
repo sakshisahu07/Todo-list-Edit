48d99ea93814865ef930db6885aad0e6ad37fec5
sakshisahu07 <sakshisahu072@gmail.com>
2024-07-30 22:44:41 -0700

    Redux Toolkit actions and payload

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import store from './Store.jsx';
 import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<Provider store={store}>
-<App />
-</Provider>
+    <Provider store={store}>
+        <App />
+    </Provider>
 )
 
 
